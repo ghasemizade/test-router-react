4848e771ed8ee5036b52bc91de7854ae5e8cd5e6
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-05-05 13:27:17 +0430

    update homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,34 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Icon, Typography } from "@mui/material";
 import React from "react";
+import '../App.css';
+import{ FaBars, }from "react-icons/fa";
 
 const HomePage = ()=> {
     return (
-        <Container maxWidth="sm">
-            <Typography style={{backgroundColor: 'Orange', height: '100vh'}}>
-                <h1>hosein ghasemizade</h1>
+        <Container maxWidth="sm" className="container-center">
+            <Typography style={{backgroundColor: '', height: 'auto'}}>
+                <div className="container-homepage">
+                    <header className="header-homepage">
+                        <div className="head-top">
+                            <Icon><FaBars/></Icon>
+                            <p className="selected-lang">English</p>
+                        </div>
+                        <div className="avatar">
+                            <img src={require('../avatar-2023.jpg')} alt="avatar" />
+                            <h3>hosein ghasemizade</h3>
+                        </div>
+                    </header>
+                    <div className="main">
+                        <div className="main-content">
+                            <h2>Welcome back!</h2>
+                            <div className="banner">
+                                <div className="banner-box">
+                                    <img src={require('../banner.jpg')} alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Typography>
         </Container>
     )
