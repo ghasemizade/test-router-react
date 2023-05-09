07a373ae493fa1762ba90ec53b302e81350e4137
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-05-09 19:23:59 +0430

    update searchbar additives

--- src/pages/Additives.jsx
+++ src/pages/Additives.jsx
@@ -1,9 +1,23 @@
+import { Container, Icon, Typography } from "@mui/material";
 import React from "react";
+import{ FaBars, FaSearch, }from "react-icons/fa";
+
 
 const Additives = ()=> {
     return (
         <div>
-            <h2>Additives page</h2>
+            <Container maxWidth="sm" className="container-center">
+                <Typography style={{backgroundColor: '', height: 'auto'}}>
+                    <div className="container-additives">
+                        <div className="search-section">
+                            <div className="search-box">
+                                <input type="text" name="search-bar" id="search-additives" className="search-additives" placeholder="Search" />
+                                <Icon><FaSearch/></Icon>
+                            </div>
+                        </div>
+                    </div>
+                </Typography>
+            </Container>
         </div>
     )
 }
